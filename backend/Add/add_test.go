516e3f1a4d71829e75f0bd81b1bdfdc832dd7288
taiti09 <taitiabe1997@gmail.com>
2024-04-18 22:06:19 +0900

    テスト

--- backend/Add/add_test.go
+++ backend/Add/add_test.go
@@ -63,3 +63,33 @@ func TestMinus(t *testing.T) {
 		})
 	}
 }
+
+func TestDouble(t *testing.T) {
+	cases := []struct {
+		name  string
+		arg1  int
+		arg2  int
+		want  int
+		iserr bool
+	}{
+		{"ケース1", 1, 1, 1, false},
+		{"ケース2", 1, 2, 2, false},
+		{"ケース3", 2, 3, 6, false},
+		{"ケース4", 4, 5, 20, false},
+		{"ケース5", 5, 6, 30, false},
+		{"ケース6", 6, 7, 42, false},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			actual, err := Double(tt.arg1, tt.arg2)
+			if actual != tt.want {
+				t.Errorf("expected : %v, but actual %v", tt.want, actual)
+			}
+
+			if (err != nil) != tt.iserr {
+				t.Errorf("expected : nil, but actual %v", err)
+			}
+		})
+	}
+}
